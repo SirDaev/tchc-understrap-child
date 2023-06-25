cf0d8af0df4e3619bf4c87205011645f57bda782
Dave Judd <52185dj@gmail.com>
2023-06-25 10:37:45 -0500

    Create page template

---
+++ page-templates/tchcfullwidthpage.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: TCHC Full Width Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<main>
+	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+		<div class="row">
+			<div class="col-md-12 content-area" id="primary">
+				<?php
+				while ( have_posts() ) {
+					the_post();
+					get_template_part( 'loop-templates/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+				}
+				?>
+			</div>
+		</div>
+	</div>
+</main>
+
+<?php
+get_footer();
